6d10c05a581acdafbc530d7eaa6e5022baead1c9
Ewerthon Kawan Gomes Ramalho <ewerthonkawan@gmail.com>
2026-09-03 21:49:21 -0300

    feat(di): register authentication data sources and repository in service locator
    - Added registration for AuthRemoteDataSource and AuthLocalDataSource in the service locator for managing authentication data.
    - Integrated AuthRepositoryImpl to facilitate interaction between local and remote data sources for authentication functionalities.

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -2,6 +2,10 @@ import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 
+import '../../features/auth/data/datasources/auth_local_data_source.dart';
+import '../../features/auth/data/datasources/auth_remote_data_source.dart';
+import '../../features/auth/data/repositories/auth_repository_impl.dart';
+import '../../features/auth/domain/repositories/auth_repository.dart';
 import '../database/db_helper.dart';
 import '../database/tables/database_table.dart';
 import '../database/tables/inspections_table.dart';
@@ -27,4 +31,21 @@ Future<void> setupServiceLocator() async {
   );
 
   sl.registerLazySingleton<Dio>(() => sl<ApiClient>().client);
+
+  // Data Sources
+  sl.registerLazySingleton<AuthRemoteDataSource>(
+    () => AuthRemoteDataSourceImpl(dio: sl<Dio>()),
+  );
+
+  sl.registerLazySingleton<AuthLocalDataSource>(
+    () => AuthLocalDataSourceImpl(storage: sl<FlutterSecureStorage>()),
+  );
+
+  // Repositories
+  sl.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(
+      remoteDataSource: sl<AuthRemoteDataSource>(),
+      localDataSource: sl<AuthLocalDataSource>(),
+    ),
+  );
 }
